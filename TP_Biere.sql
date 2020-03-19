e10d34bd2524ea8fdacf0eb64d2a6ad6fc48c948
JejeQ59 <quintinj59@gmail.com>
2020-03-19 09:44:40 +0100

    Question 5 TP Biere

--- TP_Biere.sql
+++ TP_Biere.sql
@@ -12,3 +12,6 @@ select * from ticket where DATE_VENTE between '2014-01-15' and '2014-01-17';
 -- Question 4
 select distinct NOM_ARTICLE from article as a inner join ventes as v on a.ID_ARTICLE = v.ID_ARTICLE
 where v.QUANTITE >= 50; 
+
+-- Question 5
+select * from ticket where ANNEE = 2014 and month(DATE_VENTE) = 3;
