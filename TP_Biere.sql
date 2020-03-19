df1141bfbcf443fde098209bff51a75c3c876019
JejeQ59 <quintinj59@gmail.com>
2020-03-19 09:39:55 +0100

    Question 4 TP Biere

--- TP_Biere.sql
+++ TP_Biere.sql
@@ -4,4 +4,11 @@
 select NUMERO_TICKET from ventes where ID_ARTICLE = 500;
 
 -- Question 2
-select * from ticket where DATE_VENTE = '2014-01-15'
+select * from ticket where DATE_VENTE = '2014-01-15';
+
+-- Question 3
+select * from ticket where DATE_VENTE between '2014-01-15' and '2014-01-17';
+
+-- Question 4
+select distinct NOM_ARTICLE from article as a inner join ventes as v on a.ID_ARTICLE = v.ID_ARTICLE
+where v.QUANTITE >= 50; 
