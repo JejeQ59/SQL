6d5cbcad9a1f4a4ec26b98b1d185494d599cbf79
JejeQ59 <quintinj59@gmail.com>
2020-03-20 17:00:07 +0100

    dernier commit

--- TP Nothwind.sql
+++ TP Nothwind.sql
@@ -1,5 +1,7 @@
 -- Question 1
-select count(*) from employees where city = 'Seattle';
+select (select count(*) from employees where city = 'Seattle') +
+ (select count(*) from customers where city = 'Seattle') as compte 
+, last_name, first_name FROM (SELECT * FROM employees WHERE city = "Seattle" UNION SELECT * FROM customers WHERE city = "Seattle") as employees;
 
 -- Question 2
 select last_name, first_name, email_address from employees as e inner join employee_privileges as ep on e.id = ep.employee_id 
@@ -9,3 +11,9 @@ inner join privileges as p on ep.privilege_id = p.id  where privilege_name = 'Pu
 select product_code, product_name, quantity_per_unit from products where category = 'Condiments';
 
 -- Question 4
+select * from products order by standard_cost union select * from products order by standard_cost
+
+-- Question 5
+select round(avg(standard_cost), 2) as prix_moyen, category from products where category in ('Candy', 'Dried Fruit & Nuts', 'Condiments') group by category
+
+-- Question 6
